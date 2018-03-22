8039dffa16240bccd34e03310b137a89960829fe
AVEVA\abhishek.harpanahall <abhishek.harpanahalli@aveva.com>
2018-03-22 15:04:13 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {  BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
+//import material module
+import  { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import  { MatLoginComponent } from './mat-login/mat-login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
